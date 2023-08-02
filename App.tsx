9f3380a67b01ea81bc4784446765aae0fa343b10
Waile Naeem <wailenaeem22@gmail.com>
2023-08-03 00:30:30 +0500

    app setup

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
@@ -26,12 +27,36 @@ function App(): JSX.Element {
 
 
   return (
-    <Text>Hello</Text>
+    <>
+      <StatusBar backgroundColor={"#000000"} />
+      <View style={[styles.container]}>
+        <TouchableOpacity>
+          <View style={styles.actionButton}>
+            <Text style={styles.actionButtonText}>Press Me</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  actionButton: {
+    borderRadius: 12,
+    backgroundColor: "#6A1B4d",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+  },
+  actionButtonText: {
+    fontSize: 24,
+    color: "#FFFFFF",
+    textTransform: "uppercase",
+  },
 });
 
 export default App;
